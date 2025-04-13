019f2f479d45b3b71366e2076c0044915a10f27d
Jean-Baptiste DALLE <jean-baptiste.dalle@outlook.com>
2025-04-13 23:29:18 +0200

    style(editorconfig): add .editorconfig with some default configurations

--- src/main/java/com/example/my_api/diagnostics/controller/DiagnosticController.java
+++ src/main/java/com/example/my_api/diagnostics/controller/DiagnosticController.java
@@ -1,15 +1,17 @@
 package com.example.my_api.diagnostics.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
@@ -17,6 +19,14 @@ public class DiagnosticController {
 
     @Autowired
     private HealthEndpoint healthEndpoint;
+
+    public static HttpStatus mapStatusToHttpStatus(Status status) {
+        return switch (status.getCode()) {
+            case "UP" -> HttpStatus.OK;
+            case "DOWN", "OUT_OF_SERVICE" -> HttpStatus.SERVICE_UNAVAILABLE;
+            default -> HttpStatus.INTERNAL_SERVER_ERROR;
+        };
+    }
 
     @Operation(
             summary = "Health check endpoint",
@@ -32,17 +42,9 @@ public class DiagnosticController {
             description = "Application is down or unavailable",
             content = @Content(mediaType = "text/plain", schema = @Schema(example = "DOWN")))
     @GetMapping("/health/check")
-    public ResponseEntity<String> healthCheck(){
+    public ResponseEntity<String> healthCheck() {
 
         Status status = healthEndpoint.health().getStatus();
         return ResponseEntity.status(mapStatusToHttpStatus(status)).body(status.getCode());
-    }
-
-    public static HttpStatus mapStatusToHttpStatus(Status status) {
-        return switch (status.getCode()) {
-            case "UP" -> HttpStatus.OK;
-            case "DOWN", "OUT_OF_SERVICE" -> HttpStatus.SERVICE_UNAVAILABLE;
-            default -> HttpStatus.INTERNAL_SERVER_ERROR;
-        };
     }
 }
