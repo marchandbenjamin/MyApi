4b96fd678556bb1d0030f3c17e622cf4f46c6592
Jean-Baptiste DALLE <jean-baptiste.dalle@stereograph.fr>
2025-04-15 14:08:06 +0200

    feat(keycloak): initialize postgresql and keycloak

--- src/main/java/com/example/my_api/swagger/controller/SwaggerController.java
+++ src/main/java/com/example/my_api/swagger/controller/SwaggerController.java
@@ -2,12 +2,27 @@ package com.example.my_api.swagger.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class SwaggerController {
 
+    @GetMapping("/")
+    public RedirectView rootRedirectToSwagger() {
+        return new RedirectView("/api/swagger-ui.html");
+    }
+    @GetMapping("/api")
+    public RedirectView apiRedirectToSwagger() {
+        return new RedirectView("/api/swagger-ui.html");
+    }
+
     @GetMapping("/swagger")
-    public String redirectToSwaggerUi() {
-        return "redirect:/swagger-ui/index.html";
+    public RedirectView swaggerRedirectToSwaggerUi() {
+        return new RedirectView("/api/swagger-ui.html");
+    }
+
+    @GetMapping("/api/swagger")
+    public RedirectView  apiSwaggerRedirectToSwaggerUi() {
+        return new RedirectView("/api/swagger-ui.html");
     }
 }
